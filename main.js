f330c3dd7f416343cd16192a6673706fa63b24a5
Ivan Martinez von Halle <ivanmvh@gmail.com>
2022-10-12 23:01:14 -0500

    Create popup window dynamically with js (main.js)

--- main.js
+++ main.js
@@ -1,41 +1,41 @@
-const showMobileMenu = document.querySelector('.hamburger');
-const mobileMenu = document.querySelector('.mobile-menu');
-const crossDiv = document.querySelector('.close-menu');
+const iconMobileMenu = document.querySelector('.hamburger');
+const optionsMobileMenu = document.querySelector('.mobile-menu');
+const closeMobileMenu = document.querySelector('.close-menu');
 
-showMobileMenu.addEventListener('click', () => {
-  mobileMenu.classList.toggle('open');
-  mobileMenu.style.display = 'block';
-  mobileMenu.style.height = '100vh';
-  showMobileMenu.style.visibility = 'hidden';
-  crossDiv.style.visibility = 'visible';
+iconMobileMenu.addEventListener('click', () => {
+  optionsMobileMenu.classList.toggle('open');
+  optionsMobileMenu.style.display = 'block';
+  optionsMobileMenu.style.height = '100vh';
+  iconMobileMenu.style.visibility = 'hidden';
+  closeMobileMenu.style.visibility = 'visible';
 });
 
-crossDiv.addEventListener('click', () => {
-  mobileMenu.classList.remove('open');
-  mobileMenu.style.height = '0';
-  showMobileMenu.style.visibility = 'visible';
-  crossDiv.style.visibility = 'hidden';
-  mobileMenu.style.height = '0';
+closeMobileMenu.addEventListener('click', () => {
+  optionsMobileMenu.classList.remove('open');
+  optionsMobileMenu.style.height = '0';
+  iconMobileMenu.style.visibility = 'visible';
+  closeMobileMenu.style.visibility = 'hidden';
+  optionsMobileMenu.style.height = '0';
 });
 
 window.addEventListener('resize', () => {
   if (window.innerWidth < 768) {
-    mobileMenu.classList.remove('open');
-    mobileMenu.style.display = 'none';
-    crossDiv.style.visibility = 'hidden';
-    showMobileMenu.style.visibility = 'visible';
+    optionsMobileMenu.classList.remove('open');
+    optionsMobileMenu.style.display = 'none';
+    closeMobileMenu.style.visibility = 'hidden';
+    iconMobileMenu.style.visibility = 'visible';
   } else {
-    showMobileMenu.style.visibility = 'hidden';
-    mobileMenu.style.height = 'auto';
+    iconMobileMenu.style.visibility = 'hidden';
+    optionsMobileMenu.style.height = 'auto';
   }
 });
 
-mobileMenu.addEventListener('click', () => {
-  crossDiv.style.visibility = 'hidden';
-  mobileMenu.style.display = 'none';
-  mobileMenu.style.height = '0';
-  showMobileMenu.style.visibility = 'visible';
-  mobileMenu.classList.remove('open');
+optionsMobileMenu.addEventListener('click', () => {
+  closeMobileMenu.style.visibility = 'hidden';
+  optionsMobileMenu.style.display = 'none';
+  optionsMobileMenu.style.height = '0';
+  iconMobileMenu.style.visibility = 'visible';
+  optionsMobileMenu.classList.remove('open');
 });
 
 const projectsArray = [
@@ -56,7 +56,7 @@ const projectsArray = [
     side: 'left-side',
     technologies: ['html', 'css', 'javascript'],
     technologiesDesktop: ['html', 'css', 'ruby on rails', 'javascript'],
-    technologiesModal: [
+    technologiesPopup: [
       'html',
       'css',
       'javascript',
@@ -85,7 +85,7 @@ const projectsArray = [
     side: 'right-side',
     technologies: ['html', 'ruby on rails', 'css', 'javascript'],
     technologiesDesktop: ['html', 'css', 'ruby on rails', 'javascript'],
-    technologiesModal: [
+    technologiesPopup: [
       'html',
       'css',
       'javascript',
@@ -114,7 +114,7 @@ const projectsArray = [
     side: 'left-side',
     technologies: ['html', 'css', 'javascript'],
     technologiesDesktop: ['html', 'css', 'ruby on rails', 'javascript'],
-    technologiesModal: [
+    technologiesPopup: [
       'html',
       'css',
       'javascript',
@@ -142,7 +142,7 @@ const projectsArray = [
     side: 'right-side',
     technologies: ['html', 'css', 'javascript'],
     technologiesDesktop: ['html', 'css', 'ruby on rails', 'javascript'],
-    technologiesModal: [
+    technologiesPopup: [
       'html',
       'css',
       'javascript',
@@ -204,4 +204,77 @@ for (let i = 0; i < projectsArray.length; i += 1) {
   `;
 }
 
+const popupButton = document.querySelectorAll('.project-btn');
+const popupCont = document.querySelector('.popup-container');
+
+function close() {
+  popupCont.style.display = 'none';
+}
+
+for (let index = 0; index < popupButton.length; index += 1) {
+  popupButton[index].addEventListener('click', (e) => {
+    const pos = e.target.getAttribute('id');
+
+    popupCont.style.display = 'block';
+
+    popupCont.innerHTML = `
+    <div class="popup-box">
+          <div class="popup-dialog">
+            <div class="popup-header">
+              <div>
+                <h3 class="popup-title">${projectsArray[pos].title}</h3>
+                <div class="project-data" style="margin-bottom= 150px;">
+                  <p class="bold for-mobile">${projectsArray[pos].featured[0]}</p>
+                  <p class="middot for-mobile">•</p>
+                  <p class="for-mobile">${projectsArray[pos].featured[1]}</p>
+                  <p class="middot for-mobile">•</p>
+                  <p class="for-mobile">${projectsArray[pos].featured[2]}</p>
+
+                  <p class="bold for-desktop">${projectsArray[pos].featuredDesktop[0]}</p>
+                  <p class="middot for-desktop">•</p>
+                  <p class="for-desktop">${projectsArray[pos].featuredDesktop[1]}</p>
+                  <p class="middot for-desktop">•</p>
+                  <p class="for-desktop">${projectsArray[pos].featuredDesktop[2]}</p>
+                </div>
+              </div>
+              <img src="./images/popup-img/close-popup.svg" alt="close popup image" class="close-popup" onclick="close()" width="20" height="20">
+            </div>
+            <div class="popup-content">
+              <div class="img-container">
+                <img class="for-mobile" src="${projectsArray[pos].image}" alt="project image" loading="lazy" width="641.2" height="478.183">
+                <img class="for-desktop" src="${projectsArray[pos].desktopImage}" alt="project image" loading="lazy" width="969" height="797">
+              </div>
+              <div class="description">
+                <p class="desc for-mobile">${projectsArray[pos].mobileDescr}</p>
+                <p class="desc for-desktop">${projectsArray[pos].longDescr}</p>
+                <div class="tech-box">
+                  <div class="tech">
+                    <ul class="buttons-flex" style="margin-bottom:30px">
+                      <li class="li-btn">${projectsArray[pos].technologiesPopup[0]}</li>
+                      <li class="li-btn">${projectsArray[pos].technologiesPopup[1]}</li>
+                      <li class="li-btn">${projectsArray[pos].technologiesPopup[2]}</li>
+                      <li class="li-btn">${projectsArray[pos].technologiesPopup[3]}</li>
+                      <li class="li-btn">${projectsArray[pos].technologiesPopup[4]}</li>
+                      <li class="li-btn">${projectsArray[pos].technologiesPopup[5]}</li>
+                    </ul>
+                  </div>
+                  <div class="source-box">
+                    <a class="btn-popup live" href="${projectsArray[pos].liveURL}">See Live
+                      <img src="./images/popup-img/live-link.svg" alt="live image">
+                    </a>
+                    <a class="btn-popup source" href="${projectsArray[pos].sourceURL}">See Source
+                      <img src="./images/popup-img/source-link.svg" alt="source image">
+                    </a>
+                  </div> <!-- source-box -->
+                </div> <!-- tech-box -->
+              </div> <!-- description -->
+            </div> <!-- popup-content -->
+          </div> <!-- popup-dialog -->
+        </div>  <!-- popup-box--> 
+        `;
+    const closePopup = document.querySelector('.close-popup');
+    closePopup.addEventListener('click', close);
+    popupCont.addEventListener('click', close);
+  });
+}
 
