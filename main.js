3642de0c00e919938d314e72fdf03205a5cc85b5
Ivan Martinez von Halle <ivanmvh@gmail.com>
2022-10-12 22:36:09 -0500

    Create projectsArray  with the info of the project cards.

--- main.js
+++ main.js
@@ -37,3 +37,121 @@ mobileMenu.addEventListener('click', () => {
   showMobileMenu.style.visibility = 'visible';
   mobileMenu.classList.remove('open');
 });
+
+const projectsArray = [
+  {
+    id: '1',
+    title: 'Tonic',
+    titleDesktop: 'Tonic',
+    mobileDescr:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    desktopDescr:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    longDescr:
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    featured: ['Canopy', 'Back End Dev', 2015],
+    featuredDesktop: ['Canopy', 'Back End Dev', 2015],
+    image: './images/work-sec-img/snapshot-1.svg',
+    desktopImage: './images/work-sec-img/for-desktop/snapshoot-1.svg',
+    side: 'left-side',
+    technologies: ['html', 'css', 'javascript'],
+    technologiesDesktop: ['html', 'css', 'ruby on rails', 'javascript'],
+    technologiesModal: [
+      'html',
+      'css',
+      'javascript',
+      'github',
+      'ruby',
+      'bootstraps',
+    ],
+    liveURL: 'https://ivanmvh.github.io/Portfolio-Ivan-Martinez-Microverse-Project/',
+    sourceURL: 'https://github.com/ivanmvh/Portfolio-Ivan-Martinez-Microverse-Project',
+  },
+
+  {
+    id: '2',
+    title: 'Multi-Post Stories',
+    titleDesktop: 'Multi-Post Stories',
+    mobileDescr:
+      'A convenient way to keep track of their daily routine works.',
+    desktopDescr:
+      'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    longDescr:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    featured: ['Canopy', 'Back End Dev', 2015],
+    featuredDesktop: ['Facebook', 'Full Stack Dev', 2015],
+    image: './images/work-sec-img/snapshot-2.svg',
+    desktopImage: './images/work-sec-img/for-desktop/snapshoot-2.svg',
+    side: 'right-side',
+    technologies: ['html', 'ruby on rails', 'css', 'javascript'],
+    technologiesDesktop: ['html', 'css', 'ruby on rails', 'javascript'],
+    technologiesModal: [
+      'html',
+      'css',
+      'javascript',
+      'github',
+      'ruby',
+      'bootstraps',
+    ],
+    liveURL: 'https://ivanmvh.github.io/Portfolio-Ivan-Martinez-Microverse-Project/',
+    sourceURL: 'https://github.com/ivanmvh/Portfolio-Ivan-Martinez-Microverse-Project',
+  },
+
+  {
+    id: '3',
+    title: 'Tonic',
+    titleDesktop: 'Facebook 360',
+    mobileDescr:
+      'This is a rocket booking and mission joining application.',
+    desktopDescr:
+      "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+    longDescr:
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    featured: ['Canopy', 'Back End Dev', 2015],
+    featuredDesktop: ['Facebook', 'Full Stack Dev', 2015],
+    image: './images/work-sec-img/snapshot-3.svg',
+    desktopImage: './images/work-sec-img/for-desktop/snapshoot-3.svg',
+    side: 'left-side',
+    technologies: ['html', 'css', 'javascript'],
+    technologiesDesktop: ['html', 'css', 'ruby on rails', 'javascript'],
+    technologiesModal: [
+      'html',
+      'css',
+      'javascript',
+      'github',
+      'ruby',
+      'bootstraps',
+    ],
+    liveURL: 'https://ivanmvh.github.io/Portfolio-Ivan-Martinez-Microverse-Project/',
+    sourceURL: 'https://github.com/ivanmvh/Portfolio-Ivan-Martinez-Microverse-Project',
+  },
+
+  {
+    id: '4',
+    title: 'Multi-Post Stories',
+    titleDesktop: 'Uber navigation',
+    mobileDescr: 'MyBudget is a budget manager application.',
+    desktopDescr:
+      'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    longDescr:
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    featured: ['Canopy', 'Back End Dev', 2015],
+    featuredDesktop: ['Uber', 'Lead developer', 2018],
+    image: './images/work-sec-img/snapshot-4.svg',
+    desktopImage: './images/work-sec-img/for-desktop/snapshoot-4.svg',
+    side: 'right-side',
+    technologies: ['html', 'css', 'javascript'],
+    technologiesDesktop: ['html', 'css', 'ruby on rails', 'javascript'],
+    technologiesModal: [
+      'html',
+      'css',
+      'javascript',
+      'github',
+      'ruby',
+      'bootstraps',
+    ],
+    liveURL: 'https://ivanmvh.github.io/Portfolio-Ivan-Martinez-Microverse-Project/',
+    sourceURL: 'https://github.com/ivanmvh/Portfolio-Ivan-Martinez-Microverse-Project',
+  },
+];
+
